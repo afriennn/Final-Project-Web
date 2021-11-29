b3d1a9c8fb83e977a60a1d1a3515228fb053eb0e
viadelia12 <87015833+viadelia12@users.noreply.github.com>
2021-11-30 03:30:05 +0700

    Update kategori.php

--- kategori.php
+++ kategori.php
@@ -91,25 +91,6 @@
 		</div>
 	</div>
     <!-- end header -->
-
-	<!-- search area -->
-	<div class="search-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<span class="close-btn"><i class="fas fa-window-close"></i></span>
-					<div class="search-bar">
-						<div class="search-bar-tablecell">
-							<h3>What would you like to drink?</h3>
-							<input type="text" placeholder="Keywords">
-							<button type="submit">Search <i class="fas fa-search"></i></button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end search area -->
 	
 	<!-- menu-section -->
 	<div class="breadcrumb-section breadcrumb-bg">
