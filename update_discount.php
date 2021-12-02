04297839418481da4673ffee2ed12198f512efef
afriennn <95235709+afriennn@users.noreply.github.com>
2021-12-03 01:15:09 +0700

    revisi 10

--- update_discount.php
+++ update_discount.php
@@ -77,7 +77,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- title -->
-	<title>Menu</title>
+	<title>Edit Voucher</title>
 
 	<!-- favicon -->
 	<link rel="shortcut icon" type="image/png" href="assets/img/icon.png">
