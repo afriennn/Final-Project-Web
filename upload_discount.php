5b3d547f4568d1f9031a677f9454039fd60b23d6
afriennn <95235709+afriennn@users.noreply.github.com>
2021-12-03 01:07:42 +0700

    revisi 10

--- upload_discount.php
+++ upload_discount.php
@@ -100,7 +100,6 @@
 									</ul>
 								</li>
 								<li class="current-list-item"><a href="discount_admin.php">Discount</a></li>
-								<li><a href="upload.php">Tambahkan Data</a></li>
 								<li>
 									<div class="header-icons">
 										<a href="logout.php">Logout</a>
@@ -143,9 +142,10 @@
 								<p><input type="text" name="potongan" id="potongan" required></p>
 								<label for="min_order">Minimal Order :</label>
 								<p><input type="text" name="min_order" id="min_order" required></p>
-								<p><input type="submit" name="tambah" value="Upload"><a href="shop_admin.php"><input type="submit" value="Batal"></a></p>
+								<p><input type="submit" name="tambah" value="Upload"></p>
+								<a class="cart-btn" href="discount_admin.php" style="text-decoration: none;">Batal</a>
 						</form>
-						</div>
+					</div>
 				</div>
 			</div>
 	</div>
